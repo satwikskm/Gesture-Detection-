012e62b5f87253f9b2f825194ae7703d3161a9bf
S@TwIt <56600168+satwikskm@users.noreply.github.com>
2020-04-12 13:58:49 +0530

    Add files via upload

---
+++ tocsv.py
@@ -0,0 +1,15 @@
+from scipy.misc import imread
+import numpy as np
+import pandas as pd 
+import os
+root='./gestures'
+for directory,subdirectory,files in os.walk(root):
+    for file in files:
+        print(file)
+        im=imread(os.path.join(directory,file))
+        value=im.flatten()
+        value=np.hstack((directory[8:],value))
+        df=pd.DataFrame(value).T
+        df=df.sample(frac=1)
+        with open('train_emoj.csv','a') as dataset:
+            df.to_csv(dataset, header=False, index=False)
